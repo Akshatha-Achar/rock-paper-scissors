8510944b3ab9cfef49b49e7a27e18d5945b4c877
Akshatha Achar <95159069+Akshatha-Achar@users.noreply.github.com>
2022-11-15 17:12:34 +0530

    corrected level functionality error in js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// selecting all the elements to impliment different functionalities to them.
+// Selecting the required elements
 const level = document.querySelector(".level-count");
 const score1 = document.querySelector(".score1-value");
 const score2 = document.querySelector(".score2-value");
 const restart = document.querySelector(".restart");
 const comment = document.querySelector(".display-text");
-const img1 = document.querySelector("#img1");
-const img2 = document.querySelector("#img2");
 const player_section = document.querySelector(".players-section");
 const player1_container = document.querySelector(".player1-container");
 const player2_container = document.querySelector(".player2-container");
 const player1 = document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
 
 const arr = ["Rock", "Paper", "Scissors"];
 
-// Function to get the random names from 'arr'
+// function to get the random names from 'arr'
 const getRandomCard = () => {
   const index = Math.trunc(Math.random(10) * 3);
   return arr[index];
@@ -24,22 +24,22 @@ const getRandomCard = () => {
 
 let player1_score = Number(score1.textContent);
 let player2_score = Number(score2.textContent);
-let level_count = Number(level.textContent);
 
-// Adding functionalities when the player1 button is clicked
+// When player1 button is clicked
 player1.addEventListener("click", function () {
   img1.classList.remove("hidden");
   img1.src = `src/${getRandomCard()}.png`;
   comment.classList.add("hidden");
 });
 
-// Adding functionalities when the player2 button is clicked
+// When player2 button is clicked
 player2.addEventListener("click", function () {
   img2.classList.remove("hidden");
   const c2 = getRandomCard();
   img2.src = `src/${c2}.png`;
   const c1 = img1.getAttribute("src").split("/").at(-1).split(".").at(0);
 
+  let level_count = Number(level.textContent);
   if (
     (c1 === "Rock" && c2 === "Paper") ||
     (c1 === "Paper" && c2 === "Scissors") ||
@@ -60,7 +60,7 @@ player2.addEventListener("click", function () {
     level.textContent = level_count + 1;
   } else {
     comment.classList.remove("hidden");
-
+    comment.classList.add("winner-text");
     let result = "";
     if (player2_score === player1_score) {
       result = `It's a Draw.`;
@@ -72,7 +72,6 @@ player2.addEventListener("click", function () {
     comment.textContent = `Game ended...🚩
     ${result}
     Press "RESTART" button to play`;
-    comment.classList.add("winner-text");
     img1.classList.add("hidden");
     img2.classList.add("hidden");
 
@@ -85,11 +84,12 @@ player2.addEventListener("click", function () {
   }
 });
 
-// Adding functionalities when the restart button is clicked
+// When restart button is clicked
 restart.addEventListener("click", function () {
   comment.classList.remove("winner-text");
   img1.classList.add("hidden");
   img2.classList.add("hidden");
+
   player_section.appendChild(player1_container);
   player_section.appendChild(player2_container);
 
