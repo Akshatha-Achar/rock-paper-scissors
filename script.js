db50070edcede18d3ee1b0cc9dfd8b7d02bea8cf
Akshatha Achar <95159069+Akshatha-Achar@users.noreply.github.com>
2022-11-15 17:03:02 +0530

    modified css and js script

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 "use strict";
-const player1_container = document.querySelector(".player1-container");
-const player2_container = document.querySelector(".player2-container");
-const card1 = document.querySelector(".card1");
-const card2 = document.querySelector(".card2");
+
+// selecting all the elements to impliment different functionalities to them.
+const level = document.querySelector(".level-count");
 const score1 = document.querySelector(".score1-value");
 const score2 = document.querySelector(".score2-value");
 const restart = document.querySelector(".restart");
 const comment = document.querySelector(".display-text");
-const player1 = document.querySelector(".player1");
-const player2 = document.querySelector(".player2");
-const level = document.querySelector(".level-count");
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
+const player_section = document.querySelector(".players-section");
+const player1_container = document.querySelector(".player1-container");
+const player2_container = document.querySelector(".player2-container");
+const player1 = document.querySelector(".player1");
+const player2 = document.querySelector(".player2");
 
 const arr = ["Rock", "Paper", "Scissors"];
 
+// Function to get the random names from 'arr'
 const getRandomCard = () => {
   const index = Math.trunc(Math.random(10) * 3);
   return arr[index];
@@ -22,20 +24,22 @@ const getRandomCard = () => {
 
 let player1_score = Number(score1.textContent);
 let player2_score = Number(score2.textContent);
+let level_count = Number(level.textContent);
 
+// Adding functionalities when the player1 button is clicked
 player1.addEventListener("click", function () {
   img1.classList.remove("hidden");
   img1.src = `src/${getRandomCard()}.png`;
   comment.classList.add("hidden");
 });
 
+// Adding functionalities when the player2 button is clicked
 player2.addEventListener("click", function () {
   img2.classList.remove("hidden");
   const c2 = getRandomCard();
   img2.src = `src/${c2}.png`;
   const c1 = img1.getAttribute("src").split("/").at(-1).split(".").at(0);
 
-  let level_count = Number(level.textContent);
   if (
     (c1 === "Rock" && c2 === "Paper") ||
     (c1 === "Paper" && c2 === "Scissors") ||
@@ -56,21 +60,24 @@ player2.addEventListener("click", function () {
     level.textContent = level_count + 1;
   } else {
     comment.classList.remove("hidden");
-    comment.classList.add("winner-text");
-    comment.textContent = `Game ended...🚩 ${
-      player2_score === player1_score
-        ? "It's a draw"
-        : player2_score > player1_score
-        ? "Player 2 won the game..🥇🎉"
-        : "Player 1 won the game..🏆🎉"
-    }\n\ Press Restart button to play`;
 
-    comment.style.color = "red";
+    let result = "";
+    if (player2_score === player1_score) {
+      result = `It's a Draw.`;
+    } else if (player2_score > player1_score) {
+      result = `Player2 won the game. Congrats..🥇🎉`;
+    } else {
+      result = `Player1 won the game. Congrats..🏆🎉`;
+    }
+    comment.textContent = `Game ended...🚩
+    ${result}
+    Press "RESTART" button to play`;
+    comment.classList.add("winner-text");
     img1.classList.add("hidden");
     img2.classList.add("hidden");
 
-    player1_container.removeChild(player1);
-    player2_container.removeChild(player2);
+    player_section.removeChild(player1_container);
+    player_section.removeChild(player2_container);
 
     level.textContent = 1;
     player1_score = 0;
@@ -78,12 +85,13 @@ player2.addEventListener("click", function () {
   }
 });
 
+// Adding functionalities when the restart button is clicked
 restart.addEventListener("click", function () {
   comment.classList.remove("winner-text");
   img1.classList.add("hidden");
   img2.classList.add("hidden");
-  player1_container.appendChild(player1);
-  player2_container.appendChild(player2);
+  player_section.appendChild(player1_container);
+  player_section.appendChild(player2_container);
 
   score1.textContent = 0;
   score2.textContent = 0;
